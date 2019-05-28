3a0526109826b5ba5e9f28bba6370f8dbf4ede76
Qais Yousef <qais.yousef@arm.com>
2019-05-28 13:53:16 +0100

    More naming improvements
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>

--- sched_tp/sched_tp.c
+++ sched_tp/sched_tp.c
@@ -16,7 +16,7 @@ static inline struct cfs_rq *get_group_cfs_rq(struct sched_entity *se)
 #endif
 }
 
-static void sched_load_cfs_rq(void *data, struct cfs_rq *cfs_rq)
+static void sched_pelt_cfs(void *data, struct cfs_rq *cfs_rq)
 {
 	const struct sched_avg *avg;
 	char path[PATH_SIZE];
@@ -29,7 +29,7 @@ static void sched_load_cfs_rq(void *data, struct cfs_rq *cfs_rq)
 	trace_sched_pelt_cfs(cpu, path, avg);
 }
 
-static void sched_load_rt(void *data, struct rq *rq)
+static void sched_pelt_rt(void *data, struct rq *rq)
 {
 	const struct sched_avg *avg = sched_trace_rq_avg_rt(rq);
 	int cpu = sched_trace_rq_cpu(rq);
@@ -40,7 +40,7 @@ static void sched_load_rt(void *data, struct rq *rq)
 	trace_sched_pelt_rt(cpu, avg);
 }
 
-static void sched_load_dl(void *data, struct rq *rq)
+static void sched_pelt_dl(void *data, struct rq *rq)
 {
 	const struct sched_avg *avg = sched_trace_rq_avg_dl(rq);
 	int cpu = sched_trace_rq_cpu(rq);
@@ -51,7 +51,7 @@ static void sched_load_dl(void *data, struct rq *rq)
 	trace_sched_pelt_dl(cpu, avg);
 }
 
-static void sched_load_irq(void *data, struct rq *rq)
+static void sched_pelt_irq(void *data, struct rq *rq)
 {
 	const struct sched_avg *avg = sched_trace_rq_avg_irq(rq);
 	int cpu = sched_trace_rq_cpu(rq);
@@ -62,7 +62,7 @@ static void sched_load_irq(void *data, struct rq *rq)
 	trace_sched_pelt_irq(cpu, avg);
 }
 
-static void sched_load_se(void *data, struct sched_entity *se)
+static void sched_pelt_se(void *data, struct sched_entity *se)
 {
 	void *gcfs_rq = get_group_cfs_rq(se);
 	void *cfs_rq = se->cfs_rq;
@@ -91,30 +91,30 @@ static void sched_overutilized(void *data, int overutilized, struct root_domain 
 	trace_sched_overutilized(overutilized, span);
 }
 
-static int lisa_tp_init(void)
+static int sched_tp_init(void)
 {
-	register_trace_pelt_cfs_tp(sched_load_cfs_rq, NULL);
-	register_trace_pelt_rt_tp(sched_load_rt, NULL);
-	register_trace_pelt_dl_tp(sched_load_dl, NULL);
-	register_trace_pelt_irq_tp(sched_load_irq, NULL);
-	register_trace_pelt_se_tp(sched_load_se, NULL);
+	register_trace_pelt_cfs_tp(sched_pelt_cfs, NULL);
+	register_trace_pelt_rt_tp(sched_pelt_rt, NULL);
+	register_trace_pelt_dl_tp(sched_pelt_dl, NULL);
+	register_trace_pelt_irq_tp(sched_pelt_irq, NULL);
+	register_trace_pelt_se_tp(sched_pelt_se, NULL);
 	register_trace_sched_overutilized_tp(sched_overutilized, NULL);
 
 	return 0;
 }
 
-void lisa_tp_finish(void)
+void sched_tp_finish(void)
 {
-	unregister_trace_pelt_cfs_tp(sched_load_cfs_rq, NULL);
-	unregister_trace_pelt_rt_tp(sched_load_rt, NULL);
-	unregister_trace_pelt_dl_tp(sched_load_dl, NULL);
-	unregister_trace_pelt_irq_tp(sched_load_irq, NULL);
-	unregister_trace_pelt_se_tp(sched_load_se, NULL);
+	unregister_trace_pelt_cfs_tp(sched_pelt_cfs, NULL);
+	unregister_trace_pelt_rt_tp(sched_pelt_rt, NULL);
+	unregister_trace_pelt_dl_tp(sched_pelt_dl, NULL);
+	unregister_trace_pelt_irq_tp(sched_pelt_irq, NULL);
+	unregister_trace_pelt_se_tp(sched_pelt_se, NULL);
 	unregister_trace_sched_overutilized_tp(sched_overutilized, NULL);
 }
 
 
-module_init(lisa_tp_init);
-module_exit(lisa_tp_finish);
+module_init(sched_tp_init);
+module_exit(sched_tp_finish);
 
 MODULE_LICENSE("GPL");
